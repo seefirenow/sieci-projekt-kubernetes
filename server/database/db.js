9f90933f38bf63fe45c566a5d27ad4755e423609
seefirenow <jula.napa.111115@gmail.com>
2023-01-10 00:32:53 +0100

    kubernetes

---
+++ server/database/db.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+
+const Connection = () => {
+
+    const MONGODB_URI = "mongodb://mongo:27017/auth-todo";
+    mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+    mongoose.connection.on('connected', () => {
+        console.log('Database connected Successfully');
+    })
+
+    mongoose.connection.on('disconnected', () => {
+        console.log('Database disconnected');
+    })
+
+    mongoose.connection.on('error', () => {
+        console.log('Error while connecting with the database ', error.message);
+    })
+}
+
+export default Connection;
